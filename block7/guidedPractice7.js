a48480723b303d7095cd82ee024f3a942c5b8e27
Davonne Vigil <davonnejv@gmail.com>
2023-03-20 19:56:48 -0500

    adding block7 still working on workshop but did guided practice

---
+++ block7/guidedPractice7.js
@@ -0,0 +1,31 @@
+let numOfCows = 50;
+console.log(numOfCows);
+
+//
+let phrase = "Hello World";
+console.log(phrase);
+
+//
+alert("Hello Universe");
+
+//
+let numOfUmbrellas = 10 * 3;
+console.log(numOfUmbrellas);
+
+//
+let num = 5 + 1;
+alert(num);
+
+//
+let stringOne = "Learning";
+let stringTwo = "Javascript";
+let stringThree = "Everyday";
+
+let sentence = stringOne + " " + stringTwo + " " + stringThree;
+console.log(sentence);
+
+//
+let greeting = "Hello";
+let iAm = "I am";
+let name = "Davonne";
+let message = alert(greeting + " " + iAm + " " + name);
---
+++ block7/secureTheVault.js
@@ -0,0 +1,2 @@
+let name = "Davonne";
+console.log(name);

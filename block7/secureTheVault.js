9ab8d30aeecb95565be63c745262b0835fc854dc
Davonne Vigil <davonnejv@gmail.com>
2023-03-20 21:39:38 -0500

    git commit -m "adding secureTheVault files"

--- block7/secureTheVault.js
+++ block7/secureTheVault.js
@@ -1,2 +1,17 @@
-let name = "Davonne";
-console.log(name);
+// Declaring a variable 'importantVault' to a string message
+// Declare a variable 'numOne' to 2 + 8, making the value 10
+// Declare a variable 'numTwo' to 10 + 120 / 4, making the value 40
+// Declare a variable 'numThree to 15 + 30 - 6, making the value 39
+
+//ALERT a pop up alert message to add all the variables together
+  //add them together by using a string concatenation and " " to add spaces and a dash between numbers
+
+
+const importantVault = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+let numOne = 2 + 8;
+let numTwo = 10 + 120 / 4;
+let numThree = 15 + 30 - 6;
+
+alert(numOne + " " + "-" + " " + numTwo + " " + "-" + " " + numThree);
+
+

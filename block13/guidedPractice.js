f1ba395a85687b891a321753bfe167f6982f9e23
Davonne Vigil <davonnejv@gmail.com>
2023-04-13 21:02:14 -0500

    deleted html

---
+++ block13/guidedPractice.js
@@ -0,0 +1,14 @@
+// Check if a value is even or odd
+// If a number is even, console log, "even"
+// Else, console log, "odd"
+
+const myValue = 0;
+
+if (myValue % 2 === 0) {
+    console.log("even");
+}
+
+else {
+    console.log("odd");
+}
+

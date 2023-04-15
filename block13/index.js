35d2af362900c3e57f808562c96b0d569688ab65
Davonne Vigil <davonnejv@gmail.com>
2023-04-15 15:50:06 -0500

    testing another value

---
+++ block13/index.js
@@ -0,0 +1,46 @@
+// Tab One: Is Truthy
+
+// if the value is truthy, print true
+    // if the value is falsy, print a statement that it is falsy
+
+
+
+
+
+
+
+
+//Tab Two: Number Line
+  //Given two numbers, set them to variables 
+    //set the sum of those variables to another variable named sum
+    //do an if statement if sum is greater than 100, console log "101 is greater than 100"
+
+let num1 = 99;
+let num2 = -2;
+let sum = num1 + num2;
+
+if(sum >= 0) {
+    console.log("97 is greater than 0");
+}
+
+else {
+    console.log("Ehhh your wrong");
+}
+
+
+//Tab Three: Greater Than 5
+    //Given two numbers, if both parameters are greater than or equal to 5
+      //print true
+      //else console.log false
+
+const number1 = 5;
+const number2 = 6;
+
+if (number1 && number2 >= 5) {
+    console.log(true);
+}
+
+else {
+    console.log(false);
+}
+

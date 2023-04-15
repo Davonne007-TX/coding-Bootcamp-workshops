c4e3ac2a43059ed1c8ae24f6ce03e849ff3fb4db
Davonne Vigil <davonnejv@gmail.com>
2023-04-15 16:01:13 -0500

    editing pseudocode

--- block13/index.js
+++ block13/index.js
@@ -12,35 +12,35 @@
 
 //Tab Two: Number Line
   //Given two numbers, set them to variables 
-    //set the sum of those variables to another variable named sum
-    //do an if statement if sum is greater than 100, console log "101 is greater than 100"
+    let num1 = 99;
+    let num2 = -2;
 
-let num1 = 99;
-let num2 = -2;
-let sum = num1 + num2;
+//set the sum of those variables to another variable named sum
+    let sum = num1 + num2;
 
-if(sum >= 0) {
-    console.log("97 is greater than 0");
-}
+//do an if statement if sum is greater than 100, console log "101 is greater than 100"
+    if(sum >= 0) {
+        console.log("97 is greater than 0");
+    }
 
-else {
-    console.log("Ehhh your wrong");
-}
+    else {
+        console.log("Sorry, your wrong");
+    }
 
 
 //Tab Three: Greater Than 5
-    //Given two numbers, if both parameters are greater than or equal to 5
-      //print true
-      //else console.log false
+    //Given two numbers, 
+        const number1 = 5;
+        const number2 = 6;
 
-const number1 = 5;
-const number2 = 6;
+    //if both parameters are greater than or equal to 5, print true
+         if (number1 && number2 >= 5) {
+            console.log(true);
+         }
 
-if (number1 && number2 >= 5) {
-    console.log(true);
-}
-
-else {
-    console.log(false);
-}
+   
+    //else console.log false
+        else {
+            console.log(false);
+        }
 

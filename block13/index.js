63ce1429b6cb36842b116239039aff6237070ef3
Davonne Vigil <davonnejv@gmail.com>
2023-04-16 22:57:52 -0500

    adding tab four

--- block13/index.js
+++ block13/index.js
@@ -1,7 +1,10 @@
 // Tab One: Is Truthy
+    //Given a value
 
-// if the value is truthy, print true
-    // if the value is falsy, print a statement that it is falsy
+        
+    
+   
+
 
 
 
@@ -11,25 +14,27 @@
 
 
 //Tab Two: Number Line
-  //Given two numbers, set them to variables 
-    let num1 = 99;
-    let num2 = -2;
+    //Given two numbers, set them to variables 
+        let num1 = 99;
+        let num2 = -2;
 
-//set the sum of those variables to another variable named sum
-    let sum = num1 + num2;
+    //set the sum of those variables to another variable named sum
+        let sum = num1 + num2;
 
-//do an if statement if sum is greater than 100, console log "101 is greater than 100"
-    if(sum >= 0) {
-        console.log("97 is greater than 0");
-    }
+    //do an if statement if sum is greater than 100, console log "101 is greater than 100"
+        if(sum >= 0) {
+            console.log("97 is greater than 0");
+        }
 
-    else {
-        console.log("Sorry, your wrong");
-    }
+    //else print "sorry your wrong"
+        else {
+            console.log("Sorry, your wrong");
+         }
+
 
 
 //Tab Three: Greater Than 5
-    //Given two numbers, 
+    //Given two numbers, set them to variables
         const number1 = 5;
         const number2 = 6;
 
@@ -38,9 +43,50 @@
             console.log(true);
          }
 
-   
     //else console.log false
         else {
             console.log(false);
         }
 
+/*
+//Tab Four: Pair and Compare
+    //Given two values
+        let param1A = "five";
+        let param1B = 5;
+
+    //Compare the two values using an if statement
+        //print true if values are truthy
+            if (param1A === param1B) {
+                console.log(true);
+            } 
+    //else print false
+        else {
+            console.log(false);
+        } 
+    */
+   
+
+    //Tab Four: Pair and Compare Continued.... 
+        //Wasn't too sure if this is the way I was supposed to do it, or the above code, so I did both 
+
+    //Given two sets of two values
+        let param1A = "five";
+        let param1B = "5";
+
+        let param2A = "dog";
+        let param2B = "dawg";
+    
+    //Compare the two sets and two values using an if statement 
+        //print true if at least one pair is truthy
+        if (param1A && param1B === param2A && param2B) {
+            console.log(true);
+        }
+
+        //else print false
+        else {
+            console.log(false);
+        } 
+        
+
+
+      

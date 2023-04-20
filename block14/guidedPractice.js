f61c0e5bac5a3572a2ab417ef511e1214bdb565e
Davonne Vigil <davonnejv@gmail.com>
2023-04-20 18:41:57 -0500

    more guided practice

--- block14/guidedPractice.js
+++ block14/guidedPractice.js
@@ -12,3 +12,22 @@ const cars = ["Honda", "Toyota", "Ford", "Subaru"];
     cars.unshift("Hyundai");
     cars.splice(2,0, "Mercedes");
     console.log(cars);
+
+
+ /*
+    Using loops, count how many "e"s are in the following sentence:
+
+      - "There once was a donkey named Eeyore."
+
+*/
+
+let sentence = "There once was a donkey named Eeyore";
+let count = 0; 
+
+for(let i = 0; i < sentence.length; i++) {
+    if(sentence[i] === "e" || sentence[i] === "E") {
+        count++;
+    }
+}
+
+console.log(count);
---
+++ block14/more-practice.js
@@ -0,0 +1,9 @@
+let num = 1
+while (num < 8) {
+console.log("our current number is " + num);
+num++;
+}
+
+console.log("end of loop");
+
+//

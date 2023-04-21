1ce359ac35f21849e2beaa17d29da11f3655b904
Davonne Vigil <davonnejv@gmail.com>
2023-04-21 17:29:40 -0500

    more arrays for only odds

--- block14/index.js
+++ block14/index.js
@@ -1,5 +1,7 @@
 // Block 14 Workshop: Loops and Arrays
 
+
+
 /*  Only Odds:
 
      - Write an algorithm that accepts an array of numbers and 
@@ -15,9 +17,57 @@ for(let i = 0; i < myNumbers.length; i++) {
     oddNumbers.push(myNumbers[i]);
     }
 }
+
 console.log(oddNumbers);
 
 
+// Next Set of Arrays in Only Odds
+
+/*  
+    - Declare an empty array, you will use later to console log the expected result
+    - Declare a variable with the new set of array numbers
+    - Do a for loop, starting at index  1, setting it to less than or equal to 9, and iterating by 2
+    - Do an if statement if to catch all the odd numbers 
+    - push the empty array to the declared variable with the iteration 
+    - console.log the empty array
+*/
+
+let expectedResults = [];
+let inputArrays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for(let i = 1; i <= 9; i+= 2) {
+    expectedResults.push(i);
+}
+
+console.log(expectedResults);
+
+
+
+
+//Next Arrays 
+
+/*
+    - Declare a variable with the new set of array of numbers
+    - Pop the last element of the array, (get rid of the last number)
+    - Remove the first two elements of the array, use the splice method 
+    - console log the declared variable to show the new array
+*/
+
+let newArrays = [70, 42, 55, 81, 21, 91, 34];
+newArrays.pop();
+
+newArrays.splice(0, 2);
+console.log(newArrays);
+
+
 //
+
+
+
+
+
+
+
+
 
 

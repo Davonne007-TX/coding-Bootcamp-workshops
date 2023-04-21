fb5b7b2f12295f6893b3ae9c38a3aaf3fe62b533
Davonne Vigil <davonnejv@gmail.com>
2023-04-20 19:33:33 -0500

    more js practice

--- block14/more-practice.js
+++ block14/more-practice.js
@@ -1,3 +1,5 @@
+//While Loops
+
 let num = 1
 while (num < 8) {
 console.log("our current number is " + num);
@@ -6,4 +8,26 @@ num++;
 
 console.log("end of loop");
 
-//
+//Arrays
+
+let pets = ["Maryjane", "Roxy", "Lexy", "Coco"];
+
+pets.pop();
+console.log(pets);
+
+pets.push("fish", "cat");
+console.log(pets);
+
+
+//for loops
+let alphabet = " abcdefghijklmnopqrstuvwxyz";
+for(let i = 0; i < alphabet.length; i++ ) {
+    let currentLetters = alphabet[i];
+    console.log(currentLetters);
+}
+
+
+
+
+
+

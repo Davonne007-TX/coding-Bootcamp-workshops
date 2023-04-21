6eb98105a244a74d54f438d9a1dff7fe914786a4
Davonne Vigil <davonnejv@gmail.com>
2023-04-20 19:41:12 -0500

    adding old odds

---
+++ block14/index.js
@@ -0,0 +1,18 @@
+// Block 14 Workshop: Loops and Arrays
+
+/*  Only Odds:
+
+     - Write an algorithm that accepts an array of numbers and 
+    returns a new array with only the odd numbers from the original array. 
+    If there is only one odd number in the array, return an array with that single value.
+    
+*/
+
+let oddNumbers = [];
+let myNumbers = [2, 4, 6, 8, 11, 20, 15, 22];
+for(let i = 0; i < myNumbers.length; i++) {
+    if (myNumbers[i] % 2 === 1) {
+    oddNumbers.push(myNumbers[i]);
+    }
+}
+console.log(oddNumbers);

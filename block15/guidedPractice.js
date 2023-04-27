3e5b1380a8f17183e19d06c16497f184448ef5cb
Davonne Vigil <davonnejv@gmail.com>
2023-04-27 03:59:02 -0500

    guided practice block15

---
+++ block15/guidedPractice.js
@@ -0,0 +1,27 @@
+//Declare an object name dinner
+let dinner = {
+    cheeseBurger: 12,
+    steak: 20,
+    soup: 8,
+    macAndCheese: 14,
+    unlimitedSoupAndSalad: 16
+ }
+
+//Create an array of the different foods that were eaten
+ console.log(Object.keys(dinner));
+
+
+ //Create an array of the prices of the meals 
+ console.log(Object.values(dinner));
+
+ 
+ //Add the values of the dinner object,
+   // to calculate the total cost of the meal
+const prices = Object.values(dinner);
+let totalCost = 0;
+
+for(let i = 0; i < prices.length; i++) {
+    totalCost += prices[i];
+}
+
+console.log(totalCost);

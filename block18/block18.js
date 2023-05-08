abd1545a66f69156d952471e6443e475cc99df75
Davonne Vigil <davonnejv@gmail.com>
2023-05-08 07:24:30 -0500

    adding prompt 1

---
+++ block18/block18.js
@@ -0,0 +1,46 @@
+// Block 18 Workshop: Writing Test Specifications 
+
+/* 
+    Prompt 1 - A function called "multiplication" that returns
+    the product of the two input numbers.
+
+    Unit Tests:
+    ==> Except a function to be an arrow function or with the key word function
+    ==> Except the function to take two numbers as arguments, num1 and num2
+    ==> Expect function to declare a variable that takes num1 and num2
+     and multiplies them together
+     ==> Expect function to return the variable that did the multiplication 
+    ==> Expect to console.log the solution that invokes the function, and pass 
+    the numbers 10 and 20
+    ==> The value should be a number, should be 200
+    ==> If value is not a number, do not run code
+    ==> Expect to not run code if arguments are not numbers   
+    ==> Expect to not run code if there is a syntax error
+    ==> Expect to not run code if said declarations are not let, const or var
+
+
+    Integration: 
+    ==>If value is not a number, alert a message that says "Review your code"
+
+*/
+
+
+
+
+/*Prompt 2 - A function called "concatOdds" takes two arrays of integers as arguments.
+It should return a single array that only contains the odd numbers, in ascending order, 
+from both of the arrays. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/

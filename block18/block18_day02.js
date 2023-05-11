597f51220f600fb71033732c31f0942f2e4a0f87
Davonne Vigil <davonnejv@gmail.com>
2023-05-11 16:09:15 -0500

    adding prompt attendanceCheck

--- block18/block18_day02.js
+++ block18/block18_day02.js
@@ -91,4 +91,17 @@ function frequencyAnalysis(string) {
 }
 
 
+/*
+  Attendance Check:
+
+  Define a function, attendanceCheck, that accepts a string argument
+  corresponding to a day of the week.
+
+  Using the globally-defined classRoom array, attendanceCheck should
+  return a new array with only the names of the students present on
+  the inputted day of the week.
+
+  */
+
+
  

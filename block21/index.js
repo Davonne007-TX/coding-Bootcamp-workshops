aaec51de6f433f272a726c322b841d670279fb17
Davonne Vigil <davonnejv@gmail.com>
2023-05-18 23:53:43 -0500

    adding prompt

---
+++ block21/index.js
@@ -0,0 +1,29 @@
+//Block 21 Workshop: Car Factory
+
+/*
+
+    Project Requirements:
+
+    1. Create file index.js to work in.
+
+    2. Write code that defines a constructor function called Car that,
+    creates a new object with the following properties: make, model,
+    and year.
+
+    3. Add a method called getDescription to the Car prototype, which
+    returns a string containing information about the car.
+
+    4. Define the ElectricCar function as a subclass of Car. Include
+    an additional property called range that represents the range of
+    the electric car in miles.
+
+    5. Have the ElectricCar prototype override the getDescription
+    method of the Car prototype with a new implementation that
+    includes information about the range of the electric car.
+
+    6. Create an instance of ElectricCar with the make "Tesla", 
+    model "Model S", year 2019, and range 300, and the getDescription
+    method is called on the instance. The output will be a string
+    containing the make, model, year, and range of the electric car.
+
+*/

2e5e5f246c909c644b56cff169ec42f151a4a896
Vonney <davonnejv@gmail.com>
2023-05-26 02:38:30 -0500

    adding guided practice

--- block22/script.js
+++ block22/script.js
@@ -6,51 +6,112 @@
 
 */
 
-const songContainer = document.getElementById("song-container");
-const newSongForm = document.getElementById("new-song-form");
+function main() {
+    const root = document.getElementById("root");
+    const heading1 = document.createElement("h1");
+    heading1.innerHTML = "My Music";
+    heading1.style.textAlign = "left";
+    heading1.style.fontFamily = '"Lobster", cursive';
 
-const MY_MUSIC_API = "http://fsa-async-await.herokuapp.com/";
+    root.appendChild(heading1);
+}
+ 
+main();
 
+//
 const fetchAllSongs = async () => {
     try {
-        const response = await fetch(MY_MUSIC_API);
-        const theData = await response.json();
-        return theData;
+        const response = await fetch('http://fsa-async-await.herokuapp.com/api/guided-practice/songs');
+        const data = await response.json();
+        return data;
     } catch (error) {
         console.log(error);
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const init = async () => {
-    
+function renderAllSongs(songs) {
+    const songContainer = document.getElementById("song-container");
+    songContainer.innerHTML = "";
+    songs.forEach(song => {
+        const songElement = document.createElement('div');
+        songElement.textContent = `${song.title} - ${song.artist}`;
+        songContainer.appendChild(songElement);
+    });
 }
+
+async function addNewSong(song) {
+    const response = await fetch("http://fsa-async-await.herokuapp.com/api/guided-practice/songs", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(song),
+    });
+
+    const newSong = await response.text();
+
+   return newSong;
+}
+
+function renderNewSongForm() {
+    const newSongForm = document.getElementById("new-song-form");
+    newSongForm.innerHTML = `
+    <form>
+         <label for="title">Title</label>
+         <input type="text" name="title" id="title" />
+         <label for="artist">Artist</label>
+         <input type="text" name="artist" id="artist" />
+         <label for="genre">Genre</label>
+         <input type="text" name="genre" id="genre" />
+         <label for="release-date">Release Date</label>
+         <input type="date" name="release-date" id="release-date" />
+         <button type="submit">Submit</button>
+    </form>
+    `;  
+    
+    newSongForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const title = document.getElementById("title").value;
+        const artist = document.getElementById("artist").value;
+         const genre = document.getElementById("genre").value;
+        const releaseDate = document.getElementById("release-date").value;
+  
+        const newSong = {
+            title,
+            artist,
+            genre,
+            releaseDate
+        };
+  
+        const createdSong = await addNewSong(newSong);
+        console.log('New song has been created:', createdSong);
+        });
+    }
+  
+
+async function init() {
+    const songs = await fetchAllSongs();
+    console.log('All songs:', songs);
+    
+    renderAllSongs(songs);
+    renderNewSongForm();
+}
+
+init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

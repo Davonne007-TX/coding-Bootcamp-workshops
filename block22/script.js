bbdbe15bbcaa6b54700feb4cdff47776519ed230
Vonney <davonnejv@gmail.com>
2023-05-25 22:40:49 -0500

    adding prompt

---
+++ block22/script.js
@@ -0,0 +1,8 @@
+//Guided Practice Block 22
+
+/*
+
+    In this activity, you will be working to create a website that allows users
+    to create, read, update, and delete data from a database.
+
+*/

25e65c3217067551d6ed58524655689c3de5e024
Vonney <davonnejv@gmail.com>
2023-05-25 22:47:02 -0500

    adding basic html with divs

--- block22/script.js
+++ block22/script.js
@@ -3,5 +3,7 @@
 /*
     In this activity, you will be working to create a website that allows users
     to create, read, update, and delete data from a database.
-    
+
 */
+
+console.log("test if connected ");

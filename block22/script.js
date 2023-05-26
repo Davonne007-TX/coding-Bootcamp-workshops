6d7f95618c8efca9d20bf0b7e269964e5b896334
Vonney <davonnejv@gmail.com>
2023-05-25 22:41:43 -0500

    editing prompt

--- block22/script.js
+++ block22/script.js
@@ -1,8 +1,7 @@
 //Guided Practice Block 22
 
 /*
-
     In this activity, you will be working to create a website that allows users
     to create, read, update, and delete data from a database.
-
+    
 */

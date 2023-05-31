ec9a09e6c591773314e2c285ae2bfa777989969b
Vonney <davonnejv@gmail.com>
2023-05-31 01:37:52 -0500

    reviewing js

--- block22/guidedPractice.script.js
+++ block22/guidedPractice.script.js
@@ -46,99 +46,136 @@
 
 */
 
+const MY_MUSIC_API = "http://fsa-async-await.herokuapp.com/api/guided-practice/songs";  //Is this working??? 
+
+
+
+//Function for my heading1
 function main() {
-    const root = document.getElementById("root");
+    const root = document.getElementById("main");
     const heading1 = document.createElement("h1");
     heading1.innerHTML = "My Music";
     heading1.style.textAlign = "left";
-    heading1.style.fontFamily = '"Lobster", cursive';
-
+    heading1.style.color = "white";
+    heading1.style.fontSize = "30pt";
+    heading1.style.textDecoration = "underline";
+    heading1.style.fontFamily = '"Lobster", cursive';   //this has to be wrapped in single and double quotes, since its a font style(already in quotes)
+    heading1.style.textAlign = "center";  
+    
     root.appendChild(heading1);
 }
- 
+    
 main();
 
-//Fetch All Songs
-const fetchAllSongs = async () => {
+
+//async function getAllSongs
+async function getAllSongs () {
     try {
-        const response = await fetch('http://fsa-async-await.herokuapp.com/api/guided-practice/songs');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.log(error);
+        const response = await fetch(MY_MUSIC_API); 
+        const allSongsData = await response.json(); 
+
+        console.log("Songs ", allSongsData); 
+        return allSongsData;  
+    } catch (error) {  
+        console.log("error ", error);  
     }
 }
 
-//Render All Songs
+
+//Render all songs
 function renderAllSongs(songs) {
-    const songContainer = document.getElementById("song-container");
-    songContainer.innerHTML = "";
-    songs.forEach(song => {
-        const songElement = document.createElement('div');
-        songElement.textContent = `${song.title} - ${song.artist}`;
-        songContainer.appendChild(songElement);
+    const songContainer = document.getElementById("song-container"); 
+    songContainer.innerHTML = "";  
+    songs.forEach(song => {    
+        const songElement = document.createElement("div");  
+        songElement.textContent = `${song.title}`; 
+        songContainer.appendChild(songElement);  
+        
+        songElement.style.fontFamily = '"Lobster", cursive';  
+        songElement.style.fontSize = "18pt"; 
+        songElement.style.color = "white";
+        songElement.style.textAlign = "center";
     });
 }
 
-//Add New Song
+
+//Add a new song
 async function addNewSong(song) {
-    const response = await fetch("http://fsa-async-await.herokuapp.com/api/guided-practice/songs", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(song),
-    });
+    try {
+        const response = await fetch ("http://fsa-async-await.herokuapp.com/api/guided-practice/songs", {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json",  
+            },
+            body: JSON.stringify(song),  
 
-    const newSong = await response.text();
-    return newSong;
+      });
+
+        const newSong = await response.text(); 
+        return newSong; 
+    } catch (error) {
+        console.log("Error:", error);
+       }
 }
 
-//Render New Song Form with Even Listener
-function renderNewSongForm() {
-    const newSongForm = document.getElementById("new-song-form");
-    newSongForm.innerHTML = `
-    <form>
-         <label for="title">Title</label>
-         <input type="text" name="title" id="title" />
-         <label for="artist">Artist</label>
-         <input type="text" name="artist" id="artist" />
-         <label for="genre">Genre</label>
-         <input type="text" name="genre" id="genre" />
-         <label for="release-date">Release Date</label>
-         <input type="date" name="release-date" id="release-date" />
-         <button type="submit">Submit</button>
-    </form>
-    `;  
-    
-    newSongForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        const title = document.getElementById("title").value;
-        const artist = document.getElementById("artist").value;
-        const genre = document.getElementById("genre").value;
-        const releaseDate = document.getElementById("release-date").value;
-  
-        const newSong = {
-            title,
-            artist,
-            genre,
-            releaseDate
-        };
-  
-        const createdSong = await addNewSong(newSong);
-        console.log('New song has been created:', createdSong);
-        });
-    }
-  
+
+//Render New Song Form w/ Event Listener 
+const newSongForm = document.getElementById("new-song-form");  //grabbing the new-song-form from the div html, declaring it a variable
+newSongForm.innerHTML = `
+   <form id="newFormEntry">
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" />
+        <label for="artist">Artist</label>
+        <input type="text" name="artist" id="artist" />
+        <label for="genre">Genre</label>
+        <input type="text" name="genre" id="genre" />
+        <label for="release-date">Release Date</label>
+        <input type="date" name="release-date" id="release-date" />
+        <button type="submit" id="submitButton">Submit</button>
+    </form> 
+    `; 
+
+const newFormEntry = document.getElementById("newFormEntry");
+newFormEntry.style.fontFamily = '"Lobster", cursive';
+newFormEntry.style.fontSize = "16pt";
+newSongForm.style.textAlign = "center";
+
+
+newSongForm.addEventListener("submit", async (event) => { /
+    event.preventDefault();  
+    const title = document.getElementById("title").value; 
+    const artist = document.getElementById("artist").value;
+    const genre = document.getElementById("genre").value;
+    const releaseDate = document.getElementById("release-date").value;
+
+    const newSong = {  
+        title,
+        artist,
+        genre,
+        releaseDate
+    };
+
+    // Update the form with the created song details
+    const form = document.querySelector("form");
+    form.innerHTML = ` 
+        <p>New song has been created:</p> 
+        <p>Title: ${newSong.title}</p>
+        <p>Artist: ${newSong.artist}</p>
+        <p>Genre: ${newSong.genre}</p>
+        <p>Release Date: ${newSong.releaseDate}</p>
+    `; 
+});
+
+
 //Initiate functions 
 async function init() {
-    const songs = await fetchAllSongs();
-    console.log(songs);
-    
-    renderAllSongs(songs);
-    renderNewSongForm();
+    try {
+        const songs = await getAllSongs();
+        renderAllSongs(songs);
+    } catch (error) {
+        console.log("Error:", error);
+    }
 }
-
+    
 init();
-
 
